c75ee29e13c1432612a15b35482e7bbe16af4152
KalyanMudundi <Kalyan.Varma12@gmail.com>
2015-04-23 18:10:15 -0700

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'twitterapp',
 			'username'  => 'root',
-			'password'  => 'qwe123',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => 'tapp_',
